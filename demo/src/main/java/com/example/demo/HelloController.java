360f41a9fa9d496b8e127a8c7b2d7c24136c39d3
chaaga <charu.agarwal@gmail.com>
2023-10-21 16:26:11 -0400

    adding more endpoints

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class HelloController {
 
+	HashMap <String, String> map = new HashMap<>();
+
 	@GetMapping("/")
 	public String index() {
+
 		return "Greetings from Spring Boot!";
 	}
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello "+name;
+	}
 
+
+	@PostMapping("/greeting/{id}/{age}")
+	public String greetingId(@PathVariable String id,@PathVariable String age) {
+		map.put(id,age);
+		return "Hello by pathvariable "+id;
+	}
+
+	@GetMapping("/allcallers")
+	public String greeting () {
+		return map.toString();
+	}
 }
